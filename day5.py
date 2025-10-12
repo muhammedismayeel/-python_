f68773e78f7f7a53bb9ec57ea798322bb046faed
muhammedismayeel <ismayeelismu80@gmail.com>
2025-10-12 18:13:52 +0530

    if–elif–else

---
+++ day5.py
@@ -0,0 +1,9 @@
+score = int(input("Enter your score "))
+if score>=90:
+    print("Exellent")
+elif score >=70:
+    print("Good")
+elif score >= 50:
+    print("Average")
+else :
+    print("Fail")
