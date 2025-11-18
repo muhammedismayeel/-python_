0775f7796058d1a1d1982cab11dfdc4aad6b7e57
muhammedismayeel <ismayeelismu80@gmail.com>
2025-11-18 20:20:29 +0530

    Functions in Python

---
+++ day14.py
@@ -0,0 +1,15 @@
+def bigger(a,b) :
+    if a<b :
+        print ("b is bigger")
+    else :
+        print ("a is bigger")
+bigger(40,80)
+
+def even_or_odd(n) :
+    if n%2 == 0 :
+        print ("even")
+    else :
+        print ("odd")
+
+even_or_odd(12)
+even_or_odd(7)
