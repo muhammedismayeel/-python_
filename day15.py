464c77ff0e892265f67a15d50dedbe9f7ad1ae05
muhammedismayeel <ismayeelismu80@gmail.com>
2025-11-18 20:29:07 +0530

    List Methods in Python

---
+++ day15.py
@@ -0,0 +1,13 @@
+numbers = [10, 20, 30, 40, 50]
+
+numbers.append(60)
+numbers.remove(40)
+
+print(numbers)
+
+name = ["Ali", "Rahman", "Sami", "John"]
+
+name.remove("John")
+name.insert(1,"Ameer")
+
+print(name)
